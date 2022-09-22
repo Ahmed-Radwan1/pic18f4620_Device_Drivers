e386e540bc3ec2e3bc4247c0d45c5d900345f300
AhmedRadwanElbeshbeshy <ahmedradwanibrahim1.gmail.com>
2022-09-22 16:20:06 +0200

    no thing changed

--- build/default/production/MCAL/GPIO/hal_gpio.p1.d
+++
@@ -1,9 +0,0 @@
-build/default/production/MCAL/GPIO/hal_gpio.p1:  \
-MCAL/GPIO/hal_gpio.c  \
-MCAL/GPIO/hal_gpio.h  \
-MCAL/GPIO/../device_config.h  \
-MCAL/GPIO/../compiler.h  \
-MCAL/GPIO/../mcal_std_types.h  \
-MCAL/GPIO/../std_libraries.h  \
-MCAL/GPIO/../pic18f4620.h  \
-MCAL/GPIO/hal_gpio_cfg.h 
